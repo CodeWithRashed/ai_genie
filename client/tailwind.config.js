1bf751432af99fef7fd46c96d079eec1cf2c40dd
Md Rashed Ali <tools.rashed@gmail.com>
2023-11-14 16:57:21 +0600

    font and css variable created

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-primary": "var(--color-primary)",
+        "color-secondary": "var(--color-secondary)",
+        "btn-primary-bg": "var(--btn-primary-bg)",
+        "btn-secondary-bg": "var(--btn-secondary-bg)",
+        "btn-primary-text": "var(--btn-primary-text)",
+        "btn-secondary-text": "var(--btn-secondary-text)",
+        "card-primary-bg": "var(--card-primary-bg)",
+        "card-secondary-bg": "var(--card-secondary-bg)",
+        "text-color-title": "var(--text-color-title)",
+        "text-color-subtitle": "var(--text-color-subtitle)",
+        "shadow-color": "var(--shadow-color)"
+      }
+    },
+    fontFamily:{
+      Inter: ['"Inter var", sans-serif',]
+    }
   },
   plugins: [],
 }
