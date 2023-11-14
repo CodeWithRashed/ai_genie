40c500a4ba92c280566969284279977db5fc30f2
Md Rashed Ali <tools.rashed@gmail.com>
2023-11-14 17:51:03 +0600

    react router added

---
+++ client/src/Route/Router.jsx
@@ -0,0 +1,14 @@
+import {createBrowserRouter} from "react-router-dom";
+
+export const router = createBrowserRouter ([
+    
+        {
+            path: "/",
+            element: <div>Hello</div>
+        },
+        {
+            path: "/login",
+            element: <div>Hello</div>
+        }
+
+])
