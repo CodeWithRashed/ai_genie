f649d6e7888266386c374af2b94f3e89cfa486d1
Md Rashed Ali <tools.rashed@gmail.com>
2023-11-16 21:35:25 +0600

    pricing section done

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import HomeBanner from "../Components/HomeBanner/HomeBanner";
 import FeatureSection from "../Components/Sections/FeatureSection/FeatureSection";
+import PricingSection from "../Components/Sections/PricingSection/PricingSection";
 import WhyChooseUs from "../Components/Sections/WhyChooseUs/WhyChooseUs";
 
 const HomePage = () => {
   return (
     <div>
-      <section className="h-[95vh]">
+      <section className="h-[100vh]">
         <HomeBanner></HomeBanner>
       </section>
       <section className="my-16">
@@ -13,6 +14,9 @@ const HomePage = () => {
       </section>
       <section className="my-16">
         <FeatureSection></FeatureSection>
+      </section>
+      <section className="my-16">
+        <PricingSection></PricingSection>
       </section>
     </div>
   );
